8db982699af581c634c163dd9a572bc7728df09d
Rafif Rabbani <57043130+Pify@users.noreply.github.com>
2021-06-21 19:49:04 +0700

    implementing jwt authentication & upload img

--- src/Application.kt
+++ src/Application.kt
@@ -1,19 +1,27 @@
 package com.pify
 
-import com.pify.entities.Todo
+import com.pify.authentication.JwtConfig
+import com.pify.entities.LoginBody
 import com.pify.entities.TodoDraft
-import com.pify.repository.InMemoryTodoRepository
+import com.pify.repository.InMemoryUserRepository
 import com.pify.repository.MySqlTodoRepository
 import com.pify.repository.TodoRepository
+import com.pify.repository.UserRepository
 import io.ktor.application.*
+import io.ktor.auth.*
+import io.ktor.auth.jwt.*
 import io.ktor.features.*
 import io.ktor.gson.*
 import io.ktor.http.*
+import io.ktor.http.content.*
 import io.ktor.response.*
 import io.ktor.request.*
 import io.ktor.routing.*
+import java.io.File
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+val jwtConfig = JwtConfig(System.getenv("JWT_SECRET"))
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
@@ -25,83 +33,136 @@ fun Application.module(testing: Boolean = false) {
             setPrettyPrinting()
         }
     }
+    install(Authentication) {
+        jwt {
+            jwtConfig.configureKtorFeature(this)
+        }
+    }
 
     routing {
 
         val repository: TodoRepository = MySqlTodoRepository()
+        val userRepository: UserRepository = InMemoryUserRepository()
 
         get("/") {
             call.respondText("Hello TodoList!")
         }
 
-        get("/todos") {
-            //will return all todolist
-            call.respond(repository.getAllTodos())
-        }
+        post("/login") {
+            val loginBody = call.receive<LoginBody>()
 
-        get("/todos/{id}") {
-            val id = call.parameters["id"]?.toIntOrNull()
+            val user = userRepository.getUser(loginBody.username, loginBody.password)
 
-            if (id == null) {
-                call.respond(HttpStatusCode.BadRequest, "id parameter has to be a number")
-                return@get
+            if (user == null) {
+                call.respond(HttpStatusCode.Unauthorized, "Invalid credentials !")
+                return@post
             }
 
-            val todo = repository.getTodo(id)
+            val token = jwtConfig.genereateToken(
+                JwtConfig.JwtUser(user.userId, user.username)
+            )
+            call.respond(token)
+        }
 
-            if (todo == null) {
-                call.respond(HttpStatusCode.NotFound, "found no todo for the provided id $id")
-            } else {
+        authenticate {
+            get("/me") {
+                val user = call.authentication.principal as JwtConfig.JwtUser
+                call.respond(user)
+            }
+
+            get("/todos") {
+                //will return all todolist
+                call.respond(repository.getAllTodos())
+            }
+
+            get("/todos/{id}") {
+                val id = call.parameters["id"]?.toIntOrNull()
+
+                if (id == null) {
+                    call.respond(HttpStatusCode.BadRequest, "id parameter has to be a number")
+                    return@get
+                }
+
+                val todo = repository.getTodo(id)
+
+                if (todo == null) {
+                    call.respond(HttpStatusCode.NotFound, "found no todo for the provided id $id")
+                } else {
+                    call.respond(todo)
+                }
+            }
+
+            post("/todos") {
+                //post todolist
+                val parameters = call.receiveParameters()
+                val todoDraft = TodoDraft(parameters["title"].toString(), parameters["done"].toBoolean())
+
+                val todo = repository.addTodo(todoDraft)
                 call.respond(todo)
             }
+
+            put("/todos/{id}") {
+                //edit single todolist
+                val todoDraft = call.receive<TodoDraft>()
+                val todoId = call.parameters["id"]?.toIntOrNull()
+
+                if (todoId == null) {
+                    call.respond(HttpStatusCode.BadRequest, "id parameter has to be a number!")
+                    return@put
+                }
+
+                val updated = repository.updateTodo(todoId, todoDraft)
+                if (updated) {
+                    call.respond(HttpStatusCode.OK)
+                } else {
+                    call.respond(
+                        HttpStatusCode.NotFound,
+                        "found no todo with the id $todoId"
+                    )
+                }
+            }
+
+            delete("/todos/{id}") {
+                //delete single todolist
+                val todoId = call.parameters["id"]?.toIntOrNull()
+
+                if (todoId == null) {
+                    call.respond(HttpStatusCode.BadRequest, "id parameter has to be a number!")
+                    return@delete
+                }
+
+                val removed = repository.removeTodo(todoId)
+                if (removed) {
+                    call.respond(HttpStatusCode.OK)
+                } else {
+                    call.respond(
+                        HttpStatusCode.NotFound,
+                        "found no todo with the id $todoId"
+                    )
+                }
+            }
         }
 
-        post("/todos") {
-            //post todolist
-            val todoDraft = call.receive<TodoDraft>() //receive json in shape of TodoDraft
-            val todo = repository.addTodo(todoDraft)
-            call.respond(todo)
-        }
+        var fileDescription = ""
+        var fileName = ""
+        post("/upload") {
+            val multipartData = call.receiveMultipart()
+            val params = call.receiveParameters()
 
-        put("/todos/{id}") {
-            //edit single todolist
-            val todoDraft = call.receive<TodoDraft>()
-            val todoId = call.parameters["id"]?.toIntOrNull()
-
-            if (todoId == null) {
-                call.respond(HttpStatusCode.BadRequest, "id parameter has to be a number!")
-                return@put
+            multipartData.forEachPart { part ->
+                when (part) {
+                    is PartData.FormItem -> {
+                        fileDescription = part.value
+                    }
+                    is PartData.FileItem -> {
+                        fileName = part.originalFileName as String
+                        var filebytes = part.streamProvider().readBytes()
+                        File("uploads/$fileName").writeBytes(filebytes)
+                    }
+                }
             }
 
-            val updated = repository.updateTodo(todoId, todoDraft)
-            if (updated) {
-                call.respond(HttpStatusCode.OK)
-            } else {
-                call.respond(
-                    HttpStatusCode.NotFound,
-                    "found no todo with the id $todoId"
-                )
-            }
-        }
-
-        delete("/todos/{id}") {
-            //delete single todolist
-            val todoId = call.parameters["id"]?.toIntOrNull()
-
-            if (todoId == null) {
-                call.respond(HttpStatusCode.BadRequest, "id parameter has to be a number!")
-                return@delete
-            }
-
-            val removed = repository.removeTodo(todoId)
-            if (removed) {
-                call.respond(HttpStatusCode.OK)
-            } else {
-                call.respond(
-                    HttpStatusCode.NotFound,
-                    "found no todo with the id $todoId"
-                )
-            }
+            call.respondText("$fileDescription is uploaded to 'uploads/$fileName', data : ${params["userId"]}")
         }
     }
 }
